51ac3329b4cf812f0a7fc2aa467ccbc75ceec79f
micnncim <micnncim@gmail.com>
2021-02-03 20:32:21 +0900

    Add functional options to logger (#2)

--- pkg/logging/logging.go
+++ pkg/logging/logging.go
@@ -8,16 +8,39 @@ import (
 )
 
 const (
-	symbolSuccess = "✓"
-	symbolFailure = "✗"
+	defaultPrefixInfo  = "✓"
+	defaultPrefixError = "✗"
 )
 
 type Logger struct {
 	writer io.Writer
+
+	prefixInfo  string
+	prefixError string
 }
 
 func NewLogger(w io.Writer) *Logger {
-	return &Logger{writer: w}
+	l := &Logger{
+		writer:      w,
+		prefixInfo:  defaultPrefixInfo,
+		prefixError: defaultPrefixError,
+	}
+
+	return l
+}
+
+type Option func(*Logger)
+
+func WithPrefixInfo(p string) Option {
+	return func(l *Logger) {
+		l.prefixInfo = p
+	}
+}
+
+func WithPrefixError(p string) Option {
+	return func(l *Logger) {
+		l.prefixError = p
+	}
 }
 
 func (l *Logger) printf(format string, args ...interface{}) {
@@ -32,7 +55,7 @@ func (l *Logger) Infof(format string, args ...interface{}) {
 	format = fmt.Sprintf("%%s %s", format)
 
 	a := []interface{}{
-		color.New(color.FgGreen).Sprint(symbolSuccess),
+		color.New(color.FgGreen).Sprint(l.prefixInfo),
 	}
 	a = append(a, args...)
 
@@ -47,7 +70,7 @@ func (l *Logger) Errorf(format string, args ...interface{}) {
 	format = fmt.Sprintf("%%s %s", format)
 
 	a := []interface{}{
-		color.New(color.FgRed).Sprint(symbolFailure),
+		color.New(color.FgRed).Sprint(l.prefixError),
 	}
 	a = append(a, args...)
 
